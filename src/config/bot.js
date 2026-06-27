0751bff95a2c166526c00a4a8135355a983a18a5
user1683409182-debug <user1683409182@gmail.com>
2026-06-27 12:24:25 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Getting Developed by Nightline",
         // Activity type number (0 = Playing).
         type: 0,
       },
