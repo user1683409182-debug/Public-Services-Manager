997e76b57265176dfce2d242dba13c156d5f12a7
user1683409182-debug <user1683409182@gmail.com>
2026-06-27 12:41:03 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Plays on GVRV",
+        name: "Plays on GVRV 🚙",
         // Activity type number (0 = Playing).
         type: 0,
       },
